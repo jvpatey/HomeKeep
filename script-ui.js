7e4950e1106f1f606fd3436e68abbc45916c9e1b
Jeffrey Patey <jeffreyvpatey@gmail.com>
2024-05-04 17:03:49 -0300

    fixed issue where user sign out wasn't functional

--- script-ui.js
+++ script-ui.js
@@ -1,3 +1,23 @@
+/* ------ Firebase Initialization ------- */
+
+// Import Firebase, Firestore, Auth
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js'
+import { getAuth, signOut } from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBdfiZqwh7k_iRTpxFyK2_hJy4VpS8PViU",
+    authDomain: "homekeep-site.firebaseapp.com",
+    projectId: "homekeep-site",
+    storageBucket: "homekeep-site.appspot.com",
+    messagingSenderId: "135503627537",
+    appId: "1:135503627537:web:aabcbcf703a4ec60bbb8b0",
+    measurementId: "G-EKK8D35QH8"
+  };
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+
 /* ----- Dark Mode functionality ----- */
 
 // Toggle dark mode and save the preference to local storage
@@ -109,4 +129,23 @@ document.addEventListener('DOMContentLoaded', function() {
   document.addEventListener('click', function() {
     dropdownContent.style.display = 'none';
   });
+});
+
+/* ---- Firbase Auth Sign Out JS ----- */
+
+// Function to sign out the user
+function signOutUser() {
+  signOut(auth)
+      .then(() => {
+          window.location.href = "index.html";
+      })
+      .catch((error) => {
+          console.error("Error signing out: ", error);
+      });
+}
+
+// Add event listener to the sign out link
+document.getElementById('signOutLink').addEventListener('click', function(event) {
+  event.preventDefault();
+  signOutUser();
 });
